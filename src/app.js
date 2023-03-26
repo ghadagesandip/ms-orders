a36d75a5416abc701e493bfdf129003b0d3e5838
Sandip Ghadage <ghadagesandip@gmail.com>
2023-03-26 23:10:19 +0530

    updated code

--- src/app.js
+++ src/app.js
@@ -4,12 +4,18 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 import routes from './routes/index.js';
+const port = process.env.PORT || 3000
+
 
 const app = express();
 
 // middlewares
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+app.get('/', (req, res)=>{
+    res.status(200).send({message:`App is up and running on port ${port}`})
+})
+
 app.use('/api', routes)
 
 export default app;
